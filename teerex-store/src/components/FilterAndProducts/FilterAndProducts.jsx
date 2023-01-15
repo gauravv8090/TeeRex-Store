12b74db607747b919654f01d2e4a1def3e86eaa6
gauravv8090 <gaurav8090983461@gmail.com>
2023-01-15 22:09:10 +0530

    filters also added

--- teerex-store/src/components/FilterAndProducts/FilterAndProducts.jsx
+++ teerex-store/src/components/FilterAndProducts/FilterAndProducts.jsx
@@ -10,7 +10,7 @@ const FilterAndProducts = () => {
   const [data, setData] = useState([]);
   const [red, setRed] = useState(true);
 
-  const { product, addProduct, searchedData } = useContext(ProductContext);
+  const { product, addProduct, searchedData, filterProducts,addfilteredData } = useContext(ProductContext);
   const { cart, addCart } = useContext(CartContext);
 
   // console.log(product);
@@ -20,7 +20,10 @@ const FilterAndProducts = () => {
       "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json"
     )
       .then((res) => res.json())
-      .then((res) => addProduct(res));
+      .then((res) =>{
+        addProduct(res)
+        addfilteredData(res)
+      });
   };
   useEffect(() => {
     fetchData();
@@ -36,68 +39,98 @@ const FilterAndProducts = () => {
   };
   console.log(cart, "this is cart right now");
 
+  const selectCategoryHandler = (e)=>{
+    const {checked,value}=e.target;
+    if(checked){
+     setData([...data,value]);
+     
+    }
+    else{
+       setData([...data.filter((e)=>e!=value)])
+    }
+  }
+  useEffect(()=>{
+    filterProducts(data)
+  }, [data])
+  console.log(data);
+
   return (
     <div className="FilterAndProductsBox">
       <div className="FilterDiv">
         <div>
+          {/* color filter box */}
           <h3 className="filterBox">Color</h3>
           <div className="filterBox">
             <input
               type="checkbox"
-              value={red ? "red" : ""}
-              onClick={handelCheckBox}
+              value={'Red'}
+              // onClick={handelCheckBox}
+              onChange={selectCategoryHandler}
               name=""
               id=""
             />
             <label>Red</label>
           </div>
           <div className="filterBox">
-            <input type="checkbox" name="" id="" />
+            <input type="checkbox" onChange={selectCategoryHandler}
+             value={'Blue'} name="" id="" />
             <label>Blue</label>
           </div>
           <div className="filterBox">
-            <input type="checkbox" name="" id="" />
+            <input type="checkbox" value={'Green'}
+            onChange={selectCategoryHandler} name="" id="" />
             <label>Green</label>
           </div>
         </div>
         <div>
           <h3 className="filterBox">Gender</h3>
+          {/* Gender filter box */}
           <div className="filterBox">
-            <input type="checkbox" name="" id="" />
+            <input type="checkbox" onChange={selectCategoryHandler}
+             value={'Men'} name="" id="" />
             <label>Men</label>
           </div>
           <div className="filterBox">
-            <input type="checkbox" name="" id="" />
+            <input type="checkbox" onChange={selectCategoryHandler}
+             value={'Women'} name="" id="" />
             <label>Women</label>
           </div>
         </div>
         <div>
           <h3 className="filterBox">Price</h3>
+          {/* price filter box */}
           <div className="filterBox">
-            <input type="checkbox" name="" id="" />
+            <input type="checkbox"onChange={selectCategoryHandler}
+             value={'250'} name="" id="" />
             <label>0-Rs250</label>
           </div>
           <div className="filterBox">
-            <input type="checkbox" name="" id="" />
+            <input type="checkbox" onChange={selectCategoryHandler}
+             value={'251'} name="" id="" />
             <label>Rs251-450</label>
           </div>
           <div className="filterBox">
-            <input type="checkbox" name="" id="" />
+            <input type="checkbox"onChange={selectCategoryHandler}
+             value={'450'} name="" id="" />
             <label>Rs450</label>
           </div>
         </div>
         <div>
+          {/* type filter box */}
           <h3 className="filterBox">Type</h3>
           <div className="filterBox">
-            <input type="checkbox" name="" id="" />
+            <input type="checkbox" onChange={selectCategoryHandler}
+             value={'Polo'} name="" id="" />
             <label>Polo</label>
           </div>
           <div className="filterBox">
-            <input type="checkbox" name="" id="" />
+            <input type="checkbox" onChange={selectCategoryHandler}
+             value={'Hoodie'} name="" id="" />
             <label>Hoodie</label>
           </div>
           <div className="filterBox">
-            <input type="checkbox" name="" id="" />
+            <input type="checkbox"onChange={selectCategoryHandler}
+             value={'Basic'} name="" id="" />
             <label>Basic</label>
           </div>
         </div>
--- teerex-store/src/context/productContext.js
+++ teerex-store/src/context/productContext.js
@@ -5,6 +5,7 @@ export const ProductContext = createContext();
 const ProductContextProvider = ({ children }) => {
   const [product, setProduct] = useState([]);
   const [searchedData, setSearchedData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
   const addProduct = (product) => {
     setProduct(product);
@@ -12,10 +13,46 @@ const ProductContextProvider = ({ children }) => {
   const addSearchedData = (product) => {
     setSearchedData(product);
   };
+  const addfilteredData = (product) => {
+    setFilteredData(product);
+  };
+  const filterProducts = (filterData) => {
+    let result = filteredData.filter((e) => {
+      if (filterData.includes(e.color)) {
+        return e;
+      } else if (filterData.includes(e.gender)) {
+        return e;
+      } else if (filterData.includes(e.type)) {
+        return e;
+      } else if (filterData.includes("250")) {
+        return e.price <= 250;
+      } else if (filterData.includes("251")) {
+        return e.price >= 251 && e.price <= 450;
+      } else if (filterData.includes("450")) {
+        return e.price >= 450;
+      }
+    });
+    console.log(result, "filtered result");
 
+    if (result.length > 0) {
+      setProduct(result);
+    } else {
+      setProduct(filteredData);
+    }
+    // setFilteredData( [...result] )
+    //  setProduct(result)
+  };
+  console.log(filteredData, "filtered data resukt");
   return (
     <ProductContext.Provider
-      value={{ product, addProduct, searchedData, addSearchedData }}
+      value={{
+        product,
+        addProduct,
+        searchedData,
+        addSearchedData,
+        filterProducts,
+        addfilteredData,
+      }}
     >
       {children}
     </ProductContext.Provider>
